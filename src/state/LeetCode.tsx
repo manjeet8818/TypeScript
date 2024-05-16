198ea9c9bcdbbce5d1333b9bfd7c41d8f1cb158d
ManjeetOSKY <manjeet.dahiya@osky.com.au>
2024-05-16 22:52:11 +1000

    Adding files

--- src/state/LeetCode.tsx
+++ src/state/LeetCode.tsx
@@ -16,23 +16,37 @@ const LeetCode: React.FC = () => {
   // Initialize state with an empty array
   const [results, setResults] = useState<number[][]>([]);
 
-  // Example inputs
-  const nums1 = [2, 7, 11, 15];
-  const target1 = 9;
-  const nums2 = [3, 2, 4];
-  const target2 = 6;
-  const nums3 = [3, 3];
-  const target3 = 6;
+  // // Example inputs
+  // const nums1 = [2, 7, 11, 15];
+  // const target1 = 9;
+  // const nums2 = [3, 2, 4];
+  // const target2 = 6;
+  // const nums3 = [3, 3];
+  // const target3 = 6;
 
-  // Function to calculate results and update state
+  // // Function to calculate results and update state
+  // useEffect(() => {
+  //   setResults([
+  //     twoSum(nums1, target1),
+  //     twoSum(nums2, target2),
+  //     twoSum(nums3, target3),
+  //   ]);
+  // }, [nums1, nums2, nums3]); // Include the missing dependencies in the dependency array
+
   useEffect(() => {
+    const nums1 = [2, 7, 11, 15];
+    const target1 = 9;
+    const nums2 = [3, 2, 4];
+    const target2 = 6;
+    const nums3 = [3, 3];
+    const target3 = 6;
+
     setResults([
       twoSum(nums1, target1),
       twoSum(nums2, target2),
       twoSum(nums3, target3),
     ]);
-  }, [nums1, nums2, nums3]); // Include the missing dependencies in the dependency array
-
+  }, []);
   // Empty dependency array ensures this effect runs only once
 
   return (
