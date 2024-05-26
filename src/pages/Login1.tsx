598b86a88acec46c6744fb39ed601e21203e0ba5
ManjeetOSKY <manjeet.dahiya@osky.com.au>
2024-05-26 20:39:18 +1000

    Login

--- src/pages/Login1.tsx
+++ src/pages/Login1.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../resources/css/styles/style.css";
-import YourComponent from "../resources/css/js_files/script";
+// import YourComponent from "../resources/css/js_files/script";
 
 const Login1: React.FC = () => {
   return (
