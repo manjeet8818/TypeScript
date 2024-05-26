fe62c09286c3b5a4066199e55b064faa9dbdf24c
ManjeetOSKY <manjeet.dahiya@osky.com.au>
2024-05-26 23:08:06 +1000

    Login

--- src/pages/Login1.tsx
+++ src/pages/Login1.tsx
@@ -5,7 +5,7 @@ const Login1: React.FC = () => {
   return (
     <div>
       <h2>Hello world!</h2>
-      <form action="/register-user" method="POST">
+      <form action="register-user" method="POST">
         <input type="text" name="user-name-1" id="user-name-1" />
         <input type="text" name="email-2" id="email-2" />
         <input type="submit" value="submit" />
